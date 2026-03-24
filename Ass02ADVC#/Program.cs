62542aa18b9d739c297e8d84aaa788522c7bf14f
Hazem walid <walidf106@gmail.com>
2026-03-24 04:30:43 +0200

    Add project files.

---
+++ Ass02ADVC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass02ADVC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
